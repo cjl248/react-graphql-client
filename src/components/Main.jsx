760476c4893089fa9399e04010f21054a4b87e72
github cjl248 <github cjl248@cornell.edu>
2021-01-14 18:55:14 -0500

    Querying for books correctly.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import { useQuery } from 'react-apollo'
+import gql from 'graphql-tag'
 
+const GET_BOOKS = gql`
+  {
+    books {
+      id,
+      title,
+      pages
+    }
+  }
+`
+const Books = () => {
+  const { loading, error, data } = useQuery(GET_BOOKS, { errorPolicy: 'all' })
+  if (loading) {return (<h1>{`Loading`}</h1>)}
+  if (error) {
+    console.log(error);
+    return (<h1>{`Error`}</h1>)
+  }
+  const books = data.books
+  console.log(books)
+  return books.map((book, index) => {
+    return (
+      <div className='book'>
+        {`${book.title} | ${book.pages}`}
+      </div>
+    )
+  })
+}
 
 const Main = () => {
   return (
-    <div className='main-container'>
-      {`Books`}
+    <div className='book-container'>
+      <Books />
     </div>
   )
 }
