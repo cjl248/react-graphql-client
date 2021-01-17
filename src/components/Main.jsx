c5e075712896f82eb839fa926fc4084eb177c319
github cjl248 <github cjl248@cornell.edu>
2021-01-17 09:14:46 -0500

    Adding and deleting books

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,42 +1,78 @@
 import React from 'react'
-import { useQuery } from 'react-apollo'
-import gql from 'graphql-tag'
+import { gql, useQuery, useMutation } from '@apollo/client'
+import AddBook from './AddBook.jsx'
 
-const GET_BOOKS = gql`
-  {
-    books {
-      id,
-      title,
-      pages
-      author {
+const DELETE_BOOK = gql`
+  mutation delete_book($id: ID!) {
+    delete_book(id: $id) {
+      book {
         id,
-        last_name
+        title,
+        pages,
       }
+      success
     }
   }
-`
-const Books = () => {
-  const { loading, error, data } = useQuery(GET_BOOKS, { errorPolicy: 'all' })
-  if (loading) {return (<h1>{`Loading`}</h1>)}
-  if (error) {
-    console.log(error);
-    return (<h1>{`Error`}</h1>)
+`;
+
+const Books = ({books}) => {
+
+
+  const [deleteBook] = useMutation(DELETE_BOOK)
+
+  const handleDelete = (id) => {
+    deleteBook({
+      variables: {
+        id
+      },
+      update: cache => {
+        const data = cache.writeQuery({query: DELETE_BOOK})
+      },
+      variables: {id: id}
+    })
   }
-  const books = data.books
-  console.log(books)
-  return books.map((book, index) => {
-    return (
-      <div className='book'>
-        {`Title: ${book.title} | Author: ${book.author.last_name} | Pages: ${book.pages}`}
-      </div>
-    )
-  })
+
+  const renderBooks = () => {
+    return books.map((book, index) => {
+      return (
+        <div className='book' key={book.id}>
+          <div className='title'>{book.title}</div>
+          <div className='author'>{`By: ${book.author.last_name}`}</div>
+          <div
+            className='button'
+            onClick={()=>{handleDelete(book.id)}}>
+            {`Delete`}
+          </div>
+        </div>
+      )
+    })
+  }
+
+  return renderBooks()
 }
 
-const Main = () => {
+const Main = ({ page, books }) => {
+
+  const [bookList, setBookList] = React.useState(books)
+
+  const renderMain = () => {
+    if (page === 'show-books') {
+      return (
+        <>
+          <div className='book-container-title'>{`~Books~`}</div>
+          <div className='book-container'>
+            <Books books={bookList} />
+          </div>
+        </>
+      )
+    } else if (page === 'add-book') {
+      return <AddBook books={bookList} setBookList={setBookList} />
+    }
+  }
+
   return (
-    <div className='book-container'>
-      <Books />
+    <div className='main-container'>
+      {renderMain()}
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,51 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+} from '@apollo/client'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache({
+    // resultCaching: true,
+    optimistic: true,
+    typePolicies: {
+      fields: {
+        books: {
+          keyArgs: false,
+          merge(existing, incoming) {
+            let books = []
+            if (existing && existing.books) {
+              books = books.concat(existing.books)
+            }
+            if (incoming && incoming.books) {
+              books = books.concat(incoming.books)
+            }
+            return {
+              ...incoming,
+              books,
+            }
+          }
+        }
+      }
+    }
+  }),
+});
+
+function ApolloApp() {
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  )
+}
+
+ReactDOM.render(<ApolloApp />, document.getElementById('root'));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
